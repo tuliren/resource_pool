a0f81e94ed8a66e092b09095fe97261e8c0be07e
Liren Tu <ltu@liveramp.com>
2017-03-02 15:58:59 -0800

    Adjust packages

--- src/main/java/tuliren/resource_pool/exceptions/ResourceClosureFailureException.java
+++ src/main/java/tuliren/resource_pool/exceptions/ResourceClosureFailureException.java
@@ -1,6 +1,6 @@
 package tuliren.resource_pool.exceptions;
 
-public class ResourceClosureFailureException extends ResourcePoolException {
+public class ResourceClosureFailureException extends tuliren.resource_pool.exceptions.ResourcePoolException {
   public ResourceClosureFailureException(String message, Throwable throwable) {
   }
 }
